5f3cb40ca76ac02317b5e636e3424d59ffda0715
Jozef Sivek <49898653+jozefsivek@users.noreply.github.com>
2022-09-30 19:25:59 +0200

    Add ready to use disposable base classes

---
+++ src/Radicle.Common/src/Base/AsyncDisposable.cs
@@ -0,0 +1,76 @@
+﻿namespace Radicle.Common.Base;
+
+using System;
+using System.Threading.Tasks;
+
+/// <summary>
+/// <see cref="AsyncDisposable"/> can be used to aggregate
+/// several (async)disposable objects into one as a base class
+/// or collection.
+/// </summary>
+/// <remarks>
+/// Disposing is NOT thread safe.
+/// Read https://alistairevans.co.uk/2019/10/24/net-asynchronous-disposal-tips-for-implementing-iasyncdisposable-on-your-own-types/ ,
+/// https://docs.microsoft.com/en-us/dotnet/api/system.object.finalize?view=net-5.0
+/// and
+/// https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/unmanaged .
+/// Important, do not stack async usings:
+/// https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-disposeasync#unacceptable-pattern .
+/// </remarks>
+public class AsyncDisposable : Disposable, IAsyncDisposable
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AsyncDisposable"/> class.
+    /// </summary>
+    /// <param name="disposables">Collection of
+    ///     (async)disposables.</param>
+    /// <exception cref="ArgumentNullException">Thrown
+    ///     if required parameter is <see langword="null"/>.</exception>
+    public AsyncDisposable(params IDisposable[] disposables)
+        : base(disposables)
+    {
+    }
+
+    /// <inheritdoc/>
+    public async ValueTask DisposeAsync()
+    {
+        // Perform async cleanup.
+        await this.DisposeAsyncCore().ConfigureAwait(false);
+
+        // Dispose of unmanaged resources.
+        this.Dispose(false);
+
+        // Suppress finalization.
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Actual dispose methods, as in
+    /// https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-disposeasync .
+    /// </summary>
+    /// <returns>Awaitable <see cref="ValueTask"/>.</returns>
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        if (this.Disposed)
+        {
+            return;
+        }
+
+        // release managed resources
+        foreach (IDisposable d in this.Disposables.Keys)
+        {
+            if (d is IAsyncDisposable a)
+            {
+                await a.DisposeAsync().ConfigureAwait(false);
+            }
+            else
+            {
+                d.Dispose();
+            }
+        }
+
+        /* unmanaged resources see call to Dispose(false) */
+
+        this.Disposed = true;
+    }
+}
---
+++ src/Radicle.Common/src/Base/Disposable.cs
@@ -0,0 +1,173 @@
+namespace Radicle.Common.Base;
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Radicle.Common.Check;
+
+/// <summary>
+/// <see cref="Disposable"/> can be used to aggregate
+/// several disposable objects into one as base class or collection.
+/// </summary>
+/// <remarks>
+/// Disposing is NOT thread safe.
+/// Read https://alistairevans.co.uk/2019/10/24/net-asynchronous-disposal-tips-for-implementing-iasyncdisposable-on-your-own-types/ ,
+/// https://docs.microsoft.com/en-us/dotnet/api/system.object.finalize?view=net-5.0
+/// and
+/// https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/unmanaged .
+/// </remarks>
+public class Disposable : IDisposable
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Disposable"/> class.
+    /// </summary>
+    /// <param name="disposables">Collection of disposables.</param>
+    /// <exception cref="ArgumentNullException">Thrown
+    ///     if required parameter is <see langword="null"/>.</exception>
+    public Disposable(
+            params IDisposable[] disposables)
+    {
+        foreach (IDisposable d in Ensure.Param(disposables))
+        {
+            this.Add(d);
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Disposable"/> class.
+    /// </summary>
+    /// <param name="disposables">Optional collection of disposables.</param>
+    /// <exception cref="ArgumentNullException">Thrown
+    ///     if required parameter is <see langword="null"/>.</exception>
+    public Disposable(
+            IEnumerable<IDisposable>? disposables = null)
+    {
+        foreach (IDisposable d in Ensure.Optional(disposables))
+        {
+            this.Add(d);
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether
+    /// this instance is disposed.
+    /// </summary>
+    internal bool Disposed { get; set; }
+
+    /// <summary>
+    /// Gets collection of stored disposables
+    /// which will be disposed with this instance.
+    /// </summary>
+    /// <remarks>There is no concurrent set.</remarks>
+    internal ConcurrentDictionary<IDisposable, bool> Disposables { get; } = new();
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        // dispose unmanaged resources
+        this.Dispose(true);
+
+        // suppress finalization so that no double work is done.
+        // note however here we avoid having any finalization code
+        // as unmanaged resources should be handled with safe handles
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Actual dispose methods, as in
+    /// https://docs.microsoft.com/en-us/visualstudio/code-quality/ca1063-implement-idisposable-correctly?view=vs-2019 .
+    /// </summary>
+    /// <param name="disposing">Flag determining if this method is
+    ///     deterministically called, i.e. from Dispose. If called
+    ///     from <see cref="Dispose()"/> (<see langword="true"/>)
+    ///     or from finalizer (<see langword="false"/>).</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (this.Disposed)
+        {
+            return;
+        }
+
+        // release managed resources only if disposing
+        // e.g. called from Dispose()
+        if (disposing)
+        {
+            foreach (IDisposable d in this.Disposables.Keys)
+            {
+                d.Dispose();
+            }
+        }
+
+        /*
+        no unmanaged resources to dispose
+        https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-dispose
+        */
+
+        // set large fields to null to speed up GC
+        this.Disposables.Clear();
+
+        this.Disposed = true;
+    }
+
+    /// <summary>
+    /// Add <paramref name="disposable"/> to disposables owned
+    /// by this instance which will be disposed with this instance.
+    /// </summary>
+    /// <param name="disposable">Disposable object to add.</param>
+    /// <exception cref="ArgumentNullException">Thrown if
+    ///     required parameter is <see langword="null"/>.</exception>
+    /// <exception cref="ObjectDisposedException">Thrown if this instance
+    ///     is already disposed.</exception>
+    protected void Add(IDisposable disposable)
+    {
+        this.EnsureNotDisposed();
+
+        _ = this.Disposables.TryAdd(Ensure.Param(disposable).Value, true);
+    }
+
+    /// <summary>
+    /// Remove contained <paramref name="disposable"/> and dispose it.
+    /// </summary>
+    /// <param name="disposable">Disposable object to remove
+    ///     and dispose.</param>
+    /// <exception cref="ArgumentNullException">Thrown if
+    ///     the required parameter is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="disposable"/>
+    ///     was not owned by this instance.</exception>
+    /// <exception cref="ObjectDisposedException">Thrown if this instance
+    ///     is already disposed.</exception>
+    protected void RemoveAndDispose(IDisposable disposable)
+    {
+        Ensure.Param(disposable).Done();
+
+        this.EnsureNotDisposed();
+
+        if (this.Disposables.TryRemove(disposable, out _))
+        {
+            disposable.Dispose();
+        }
+        else
+        {
+            throw new ArgumentException(
+                    "Disposable was not found",
+                    nameof(disposable));
+        }
+    }
+
+    /// <summary>
+    /// Ensure this instance is not disposed.
+    /// </summary>
+    /// <param name="message">Custom optional message to use
+    ///     in place of the default generic message.</param>
+    /// <exception cref="ObjectDisposedException">Thrown
+    ///     if this instance is disposed.</exception>
+    protected void EnsureNotDisposed(string? message = null)
+    {
+        if (this.Disposed)
+        {
+            message ??= $"{this.GetType().Name} is already disposed";
+
+            throw new ObjectDisposedException(null, message);
+        }
+    }
+}
